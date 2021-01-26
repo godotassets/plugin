cd49ac1d28d476e35426f9725e3262db0dcd8380
Brian Uptagrafft <upta@outlook.com>
2021-01-25 18:12:56 -0800

    Updates to Login verbiage

--- godot-assets-integration/LinkButton.gd
+++ godot-assets-integration/LinkButton.gd
@@ -1,4 +1,4 @@
 extends LinkButton
 
 func _on_LinkButton_pressed():
-	OS.shell_open("https://www.godotassets.com/purchases/plugin")
+	OS.shell_open("https://github.com/godotassets/plugin/blob/main/README.md")
